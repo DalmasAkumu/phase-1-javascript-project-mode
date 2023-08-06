64177c9b4e74344cee1c90b39192fd75932c98f8
Dalmas <akumuodhiambo@gmail.com>
2023-08-06 14:41:19 +0300

    Progress

--- fetch.js
+++ fetch.js
@@ -1,19 +1,33 @@
-document.getElementById('review-form').addEventListener("click", function(){
+document.getElementById('review-form').addEventListener("clicked", function(){
     alert("click")
 })
-//reviewForm.addEventListener("submit", handleReviewSubmission);
+reviewForm.addEventListener("submit", handleReviewSubmission);
 
 
-
-
-
-fetch('https://dailymed.nlm.nih.gov/dailymed/services/')
-.then(res =>{
-    return res.json();
-})
- .then(data=>{
-    console.log(data);
- })
- .catch(error=> console.log(error));
-
-
+  async function fetchData() {
+    const url = 'https://drug-info-and-price-history.p.rapidapi.com/1/druginfo?drug=advil'; 
+  
+    try {
+      const response = await fetch(url);
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  
+  fetchData();
+    
+    
+    
+    /*const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
