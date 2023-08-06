6e4662b1a523311ef98e6e002ea91dab9f562d56
Dalmas <akumuodhiambo@gmail.com>
2023-08-06 17:06:38 +0300

    JS and HTML updates

--- fetch.js
+++ fetch.js
@@ -1,33 +1,20 @@
-document.getElementById('review-form').addEventListener("clicked", function(){
-    alert("click")
-})
-reviewForm.addEventListener("submit", handleReviewSubmission);
+document.addEventListener('DOMContentLoaded', function(){
 
+    const myform = document.getElementById('review-form');
+    myform.addEventListener("submit", (e)=>{
+        e.preventDefault();
 
-  async function fetchData() {
-    const url = 'https://drug-info-and-price-history.p.rapidapi.com/1/druginfo?drug=advil'; 
-  
-    try {
-      const response = await fetch(url);
-  
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-  
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  }
-  
-  fetchData();
-    
-    
-    
-    /*const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+        console.log('Form has been submitted!');
+
+    })
+
+    const btn = document.getElementById('cart');
+    btn.addEventListener("click", function(){
+        alert("clicked")
+    })
+
+    //Fetch API for Website
+
+    fetch("https://drug-info-and-price-history.p.rapidapi.com/1/druginfo?drug=advil")
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));
